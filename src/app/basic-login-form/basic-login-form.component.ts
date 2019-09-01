dc17fa086fa0616751b83d566de99edd3e233c4b
thiago-glauco <thiagoglauco@yahoo.com.br>
2019-09-01 20:51:42 -0300

    2 files modified

--- src/app/basic-login-form/basic-login-form.component.ts
+++ src/app/basic-login-form/basic-login-form.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { auth } from 'firebase/app';
-import { User } from '../shared/user';
-import { Address } from '../shared/address';
-import { UserPersonalData } from '../shared/user-personal-data';
+import { User } from '../configs/user';
+import { Address } from '../configs/address';
+import { UserPersonalData } from '../configs/user-personal-data';
 
 @Component({
   selector: 'app-basic-login-form',
@@ -11,8 +11,10 @@ import { UserPersonalData } from '../shared/user-personal-data';
   styleUrls: ['./basic-login-form.component.css']
 })
 export class BasicLoginFormComponent implements OnInit {
+  //firebase variables
   fbUser: firebase.User;
-    //Form variables:
+  
+  //Form variables:
   hidePassword: boolean = true;
   user: User = new User();
 
@@ -21,6 +23,36 @@ export class BasicLoginFormComponent implements OnInit {
   ngOnInit() {
     this.fbUser = this.angularFireAuth.auth.currentUser //Obtem o valor do objeto current user;
     console.dir(this.fbUser);
+  }
+
+  createUser() {
+    if (!this.fbUser ) {
+      this.angularFireAuth.auth.createUserWithEmailAndPassword( this.user.email, this.user.password )
+        .then( ( userCredential ) => {
+          console.dir(userCredential);
+          this.fbUser = userCredential.user;
+          this.fbUser.sendEmailVerification()
+            .then( (info) => {
+              console.log("email send");
+              console.dir(info);
+            })
+            .catch((error) => {
+              console.dir(error);
+            });
+        } )
+        .catch(function(error) {
+          // Handle Errors here.
+          let errorCode = error.code;
+          let errorMessage = error.message;
+          if (errorCode == 'auth/weak-password') {
+            alert('The password is too weak.');
+          } else {
+            alert(errorMessage);
+          }
+          console.log(error);
+        });
+        this.angularFireAuth.auth.signOut();
+    }
   }
 
 }

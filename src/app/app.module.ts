6030bc944889ab4578d58282f9731873d0896158
thiago-glauco <thiagoglauco@yahoo.com.br>
2019-09-01 18:34:50 -0300

    basic setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+
+//firebase modules
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireStorageModule } from '@angular/fire/storage';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -10,8 +14,10 @@ import { HelloComponent } from './hello.component';
 import {firebaseConfig} from './configs/firebase-config';
 @NgModule({
   imports:      [ BrowserModule, FormsModule,
-  AngularFireModule.initializeApp(firebaseConfig),
-    AngularFireDatabaseModule ],
+  AngularFireModule.initializeApp(firebaseConfig, 'login-testes'),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule, ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })

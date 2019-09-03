9bbb59725aca6e26bde334c426cc0f5ea654eacb
thiago-glauco <thiagoglauco@yahoo.com.br>
2019-09-03 16:12:58 -0300

    2 files modified

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -26,32 +26,49 @@ export class LoginFormComponent implements OnInit {
   }
 
   loginUser() {
-    this.angularFireAuth.auth.signInWithEmailAndPassword(this.user.email, this.user.password)
-    .then( (result ) => {
-      console.dir("login success");
-      console.dir( result );
-      if(this.angularFireAuth.auth.currentUser.emailVerified){
-        this.fbUser = this.angularFireAuth.auth.currentUser;
-      } else {
-        //usuário não verificado
-        alert("Usuário não ativado. Enviamos um link para ativação da sua conta. Por favor acesse seu e-mail e faça a verificação");
-      }
-    } )
-    .catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.log("Login error: " + error.code + " " + error.message);
-      alert(`Ocorreu um erro em seu login: ${error.code} \n ${error.message}`);
-      // ...
-    });
+    //verifica se há sessão válida de usuário. Se houver,envia mensagem ao usuário solicitando logout da sessão anterior
+    //antes de efetuar um novo login
+    if ( !this.fbUser ) {
+      this.angularFireAuth.auth.signInWithEmailAndPassword(this.user.email, this.user.password)
+      .then( (result ) => {
+        console.dir("login success");
+        console.dir( result );
+        if(this.angularFireAuth.auth.currentUser.emailVerified){
+          this.fbUser = this.angularFireAuth.auth.currentUser;
+        } else {
+          //usuário não verificado
+          alert("Usuário não ativado. Enviamos um link para ativação da sua conta. Por favor acesse seu e-mail e faça a verificação");
+        }
+      } )
+      .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log("Login error: " + error.code + " " + error.message);
+        alert(`Ocorreu um erro em seu login: ${error.code} \n ${error.message}`);
+        // ...
+      });
+    } else {
+      alert( `O usuário ${this.fbUser.email} ainda está com sessão válida.\nEfetue o logout do usuário antes de realizar um novo login.`);
+    }
   }
 
   logOut( ) {
-    this.angularFireAuth.auth.signOut()
-    .then( ( ) => {
-      this.fbUser = this.angularFireAuth.auth.currentUser;
-    });
+    //verifica se a sessão do usuário ainda é válida.
+    //se for, finaliza a sessão. Se não for, apenas atribui
+    //o valor undef para this.fbUser
+    if ( this.angularFireAuth.auth.currentUser ) {
+      this.angularFireAuth.auth.signOut()
+      .then( ( ) => {
+        this.fbUser = this.angularFireAuth.auth.currentUser;
+      })
+      .catch( (error) => {
+        console.dir(error);
+        alert("Não foi possível encerrar a sessão.")
+      })
+    } else {
+      this.fbUser = null;
+    }
   }
 
 }

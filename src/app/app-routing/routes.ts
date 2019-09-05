34256187691281db4c748325fb892f41c11efe1b
thiago-glauco <thiagoglauco@yahoo.com.br>
2019-09-05 11:23:33 -0300

    1 file modified

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../views/home/home.component';
+import { LoginComponent } from '../views/login/login.component';
 import { AppRoutingModule } from '../app-routing/app-routing.module';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'login', redirectTo: '/login', pathMatch: 'full'},
+  { path: 'login', component: LoginComponent},
   { path: '', redirectTo: '/login', pathMatch: 'full'}
 ];

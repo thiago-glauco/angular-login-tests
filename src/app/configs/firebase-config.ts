d473a7e09a47b0fd91bd0f455715167798d8c687
thiago-glauco <thiagoglauco@yahoo.com.br>
2019-09-24 20:59:27 -0300

    bucket

--- src/app/configs/firebase-config.ts
+++ src/app/configs/firebase-config.ts
@@ -3,7 +3,7 @@
     authDomain: "login-testes-56c38.firebaseapp.com",
     databaseURL: "https://login-testes-56c38.firebaseio.com",
     projectId: "login-testes-56c38",
-    storageBucket: "",
+    storageBucket: "login-testes-56c38.appspot.com",
     messagingSenderId: "929187106382",
     appId: "1:929187106382:web:ceb020e443b08e1f"
   };
